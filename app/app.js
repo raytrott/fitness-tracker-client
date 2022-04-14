41f11715569f79a45581dd5e5f15be3b7834d47e
raytrott <raytrott2@gmail.com>
2022-04-13 22:47:46 -0400

    Update authorization process files

--- app/app.js
+++ app/app.js
@@ -1,9 +1,15 @@
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
+const authEvents = require('./auth/events.js')
+const trackerEvents = require('./tracker/events.js')
 
-// use require without a reference to ensure a file is bundled
-// require('./example')
-
+// Auth
 $(() => {
-  // your JS code goes here
+  $('#sign-up-form').on('submit', authEvents.onSignUp)
+  $('#sign-in-form').on('submit', authEvents.onSignIn)
+  $('#change-password-form').on('submit', authEvents.onChangePassword)
+  $('#sign-out-button').on('click', authEvents.onSignOut)
+})
+
+// Fitness Tracker
+$(() => {
+  $('#add-workout').on('submit', trackerEvents.onAddWorkout)
 })
---
+++ app/auth/ui.js
@@ -0,0 +1,52 @@
+const store = require('../store.js')
+
+const onSignUpSuccess = function () {
+    $('#auth-display').html('<p>User sign up successful! User not signed in, please make sure to do so before proceeding.</p>')
+    $('form').trigger('reset')
+}
+
+const onSignUpFailure = function () {
+    $('#auth-display').html('<p>Error when signing up</p>')
+}
+
+const onSignInSuccess = function (response) {
+    $('#auth-display').html('<p class="display-text">User sign in successful!</p>')
+    $('.display-text').fadeOut(5000)
+    $('form').trigger('reset')
+    store.user = response.user
+}
+
+const onSignInFailure = function () {
+    $('#auth-display').html('<p>Error when signing in</p>')
+}
+
+const onChangePasswordSuccess = function () {
+    $('#auth-display').html('<p class="display-text">Password changed successfully!</p>')
+    $('.display-text').fadeOut(5000)
+    $('form').trigger('reset')
+}
+
+const onChangePasswordFailure = function () {
+    $('#auth-display').html('<p>Error when changing password</p>')
+}
+
+const onSignOutSuccess = function () {
+    $('#auth-display').html('<p class="display-text">Sign out successful!</p>')
+    $('.display-text').fadeOut(5000)
+    $('form').trigger('reset')
+}
+
+const onSignOutFailure = function () {
+    $('#auth-display').html('<p>Error signing out</p>')
+}
+
+module.exports = {
+    onSignUpSuccess,
+    onSignUpFailure,
+    onSignInSuccess,
+    onSignInFailure,
+    onChangePasswordSuccess,
+    onChangePasswordFailure,
+    onSignOutSuccess,
+    onSignOutFailure
+}
